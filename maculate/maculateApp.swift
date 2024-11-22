63371a40c2b64481d5327c62e134095b20112e6f
Evan Boehs <evan@boehs.org>
2024-11-21 22:34:43 -0500

    init swift

---
+++ maculate/maculateApp.swift
@@ -0,0 +1,17 @@
+//
+//  maculateApp.swift
+//  maculate
+//
+//  Created by Evan Boehs on 11/21/24.
+//
+
+import SwiftUI
+
+@main
+struct maculateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ maculateTests/maculateTests.swift
@@ -0,0 +1,17 @@
+//
+//  maculateTests.swift
+//  maculateTests
+//
+//  Created by Evan Boehs on 11/21/24.
+//
+
+import Testing
+@testable import maculate
+
+struct maculateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
