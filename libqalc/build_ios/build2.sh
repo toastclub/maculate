54668a3e24795979bd9a1e0e419db8ab37bc5b1d
Evan Boehs <evan@boehs.org>
2025-01-03 20:45:17 -0500

    progress, as it turns out this stuff like gmp was build for macOS so that's the next problem

--- libqalc/build_ios/build2.sh
+++ libqalc/build_ios/build2.sh
@@ -2,17 +2,21 @@
 
 set -e
 
-# Define versions and URLs for dependencies using two arrays
 deps_names=(
-    #"libiconv" "gettext"
-    "libxml2" "mpfr" "gmp" "icu")
+    #"libiconv"
+    #"gettext"
+    #"libxml2"
+    #"gmp"
+    #"mpfr"
+    #"icu"
+)
 deps_urls=(
     #"https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.16.tar.gz"
     #"https://ftp.gnu.org/pub/gnu/gettext/gettext-0.21.1.tar.gz"
-    "https://download.gnome.org/sources/libxml2/2.13/libxml2-2.13.5.tar.xz"
-    "https://www.mpfr.org/mpfr-4.2.1/mpfr-4.2.1.tar.gz"
-    "https://gmplib.org/download/gmp/gmp-6.3.0.tar.xz"
-    "https://github.com/unicode-org/icu/releases/download/release-72-1/icu4c-72_1-src.tgz"
+    #"https://download.gnome.org/sources/libxml2/2.13/libxml2-2.13.5.tar.xz"
+    #"https://gmplib.org/download/gmp/gmp-6.3.0.tar.xz"
+    #"https://www.mpfr.org/mpfr-4.2.1/mpfr-4.2.1.tar.gz"
+    #"https://github.com/unicode-org/icu/releases/download/release-72-1/icu4c-72_1-src.tgz"
 )
 
 # Define install and build directories
@@ -52,19 +56,53 @@ configure_and_build_cmake() {
 
     # Enter the extracted directory
     cd "${BUILD_DIR}/${name}"/*/
+    if [[ "$name" == "icu" ]]; then
+        cp -r source/* .
+    fi
 
     # Configure and build
     if [[ -f "CMakeLists.txt" ]]; then
         mkdir -p build && cd build
-        cmake .. \
-            -DCMAKE_TOOLCHAIN_FILE=../../../ios-cmake/ios.toolchain.cmake \
-            -DPLATFORM=OS64 \
-            -GXcode \
-            -DCMAKE_INSTALL_PREFIX="${prefix}"
+
+        # Special handling for libxml2
+        if [[ "$name" == "libxml2" ]]; then
+            echo "Compiling libxml2..."
+            sed -i '' 's/check_function_exists(getentropy HAVE_GETENTROPY)/set(HAVE_DECL_GETENTROPY OFF)/' ../CMakeLists.txt
+            cmake .. \
+                -DCMAKE_TOOLCHAIN_FILE=../../../ios-cmake/ios.toolchain.cmake \
+                -DPLATFORM=OS64 \
+                -GXcode \
+                -DCMAKE_INSTALL_PREFIX="${prefix}"\
+                -DLIBXML2_WITH_PROGRAMS=OFF \
+                -DLIBXML2_WITH_MODULES=OFF \
+                -DLIBXML2_WITH_ICONV=OFF \
+                -DLIBXML2_WITH_PYTHON=OFF \
+        else
+            cmake .. \
+                -DCMAKE_TOOLCHAIN_FILE=../../../ios-cmake/ios.toolchain.cmake \
+                -DPLATFORM=OS64 \
+                -GXcode \
+                -DCMAKE_INSTALL_PREFIX="${prefix}"
+        fi
+
         cmake --build . --config Release
         cmake --install . --config Release
     else
-        ./configure --host=arm-apple-darwin --prefix="${prefix}" --enable-static --disable-shared
+        if [[ "$name" == "gmp" ]]; then
+            CPPFLAGS="-fembed-bitcode -arch arm64" ./configure --disable-assembly --host=arm-apple-darwin --prefix="${prefix}" --enable-static --disable-shared --with-mp-limb-size=64
+        elif [[ "$name" == "mpfr" ]]; then
+            ./configure \
+                --host=arm-apple-darwin \
+                --prefix="${prefix}" \
+                --enable-static \
+                --disable-shared \
+                --with-gmp="${INSTALL_DIR}/ios/gmp" \
+                LDFLAGS="-arch arm64" \
+                CFLAGS="-arch arm64" \
+                CPPFLAGS="-fembed-bitcode -arch arm64"
+        else
+            ./configure --host=arm-apple-darwin --prefix="${prefix}" --enable-static --disable-shared
+        fi
         make
         make install
     fi
@@ -83,21 +121,50 @@ done
 VERSION="5.3.0"
 REPO="https://github.com/qalculate/libqalculate"
 
-git clone --branch v${VERSION} ${REPO}.git ${SRC_DIR}/libqalculate
+#git clone --branch v${VERSION} ${REPO}.git ${SRC_DIR}/libqalculate
 cd ${SRC_DIR}/libqalculate
 
-mkdir -p ${BUILD_DIR}/ios/libqalculate && cd ${BUILD_DIR}/ios/libqalculate
+mkdir -p ${BUILD_DIR}/ios/libqalculate
 
-cmake -DCMAKE_TOOLCHAIN_FILE=../../../ios-cmake/ios.toolchain.cmake \
-      -DPLATFORM=OS64 \
-      -GXcode \
-      -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/ios/libqalculate \
-      -DCMAKE_FIND_ROOT_PATH="${INSTALL_DIR}/ios" \
-      -DICU_ROOT=${INSTALL_DIR}/ios/icu \
-      -DGMP_ROOT=${INSTALL_DIR}/ios/gmp \
-      -DMPFR_ROOT=${INSTALL_DIR}/ios/mpfr \
-      ${SRC_DIR}/libqalculate
+echo "Compiling libqalculate..."
 
-cmake --build . --config Release --target install
+# replace instances of libtoolize with glibtoolize, only if its not already done
+if ! grep -q "glibtoolize" autogen.sh; then
+    sed -i '' 's/libtoolize/glibtoolize/' autogen.sh
+fi
+INCLUDE_DIRS=(
+    "${INSTALL_DIR}/ios/gmp/include"
+    "${INSTALL_DIR}/ios/mpfr/include"
+    "${INSTALL_DIR}/ios/libxml2/include"
+    "${INSTALL_DIR}/ios/gettext/include"
+    "${INSTALL_DIR}/ios/libiconv/include"
+    "${INSTALL_DIR}/ios/icu/include"
+)
+LIB_DIRS=(
+    "${INSTALL_DIR}/ios/gmp/lib"
+    "${INSTALL_DIR}/ios/mpfr/lib"
+    "${INSTALL_DIR}/ios/libxml2/lib"
+    "${INSTALL_DIR}/ios/gettext/lib"
+    "${INSTALL_DIR}/ios/libiconv/lib"
+    "${INSTALL_DIR}/ios/icu/lib"
+)
+
+export CFLAGS="-fembed-bitcode -arch arm64 $(printf -- '-I%s ' "${INCLUDE_DIRS[@]}")"
+export LDFLAGS="-arch arm64 $(printf -- '-L%s ' "${LIB_DIRS[@]}")"
+export CPPFLAGS="-fembed-bitcode -arch arm64 $(printf -- '-I%s ' "${INCLUDE_DIRS[@]}")"
+export NOCONFIGURE=1
+./autogen.sh
+./configure \
+    --host=arm-apple-darwin \
+    --prefix="${INSTALL_DIR}/ios/libqalculate"\
+    --enable-static \
+    --disable-shared \
+    --with-icu=no \
+    --with-readline=no
+
+
+
+make -j$(sysctl -n hw.ncpu)
+
 
 echo "Build process completed successfully."
